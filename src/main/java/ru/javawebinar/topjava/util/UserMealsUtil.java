9a5177349a372a0fbb42e59d55857eaa7c0e4278
Sergey Nikolaev <nikolaev.s@nictt.ru>
2020-01-28 14:39:15 +0300

    Added implementation of filteredByCycles() method through a two loops + refactoring of sortByDateTime() method

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,21 +3,21 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Objects.nonNull;
+import static ru.javawebinar.topjava.util.TimeUtil.isBetweenInclusive;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> meals = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
@@ -30,9 +30,34 @@ public class UserMealsUtil {
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
-    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime,
+                                                            LocalTime endTime, int caloriesPerDay) {
+        List<UserMealWithExcess> userMealWithExcessList = new ArrayList<>();
+        Map<LocalDate, AtomicInteger> mapCaloriesPerDate = new ConcurrentHashMap<>();
+        sortByDateTime(meals);
+        // Counting calories per date
+        meals.forEach(userMeal -> {
+            LocalDate currentDate = userMeal.getDateTime().toLocalDate();
+            if (!mapCaloriesPerDate.containsKey(currentDate)) {
+                mapCaloriesPerDate.put(currentDate, new AtomicInteger(0));
+            }
+            mapCaloriesPerDate.get(currentDate).addAndGet(userMeal.getCalories());
+        });
+        // Filter the data and fill in the UserMealWithExcess list
+        meals.forEach(userMeal -> {
+            LocalDate currentDate = userMeal.getDateTime().toLocalDate();
+            if (isBetweenInclusive(userMeal.getDateTime().toLocalTime(), startTime, endTime)) {
+                userMealWithExcessList.add(
+                        new UserMealWithExcess(
+                                userMeal.getDateTime(),
+                                userMeal.getDescription(),
+                                userMeal.getCalories(),
+                                mapCaloriesPerDate.get(currentDate).intValue() > caloriesPerDay
+                        )
+                );
+            }
+        });
+        return userMealWithExcessList;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -44,12 +69,10 @@ public class UserMealsUtil {
      * Sorts UserMeal list by DateTime
      *
      * @param meals UserMeal list
-     * @return UserMeal list
      */
-    private static List<UserMeal> sortByDateTime(List<UserMeal> meals) {
+    private static void sortByDateTime(List<UserMeal> meals) {
         if (nonNull(meals)) {
             meals.sort(Comparator.comparing(UserMeal::getDateTime));
         }
-        return meals;
     }
 }
